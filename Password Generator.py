c52368c02b0bda8f54b7c39c27d23266b5979df7
Meyer <laurynemma@yahoo.com>
2024-03-14 13:38:02 -0400

    Enter Pass - Run IN Terminal

--- Password Generator.py
+++ Password Generator.py
@@ -3,7 +3,9 @@
 # Program: Password Generator
 # Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
 
+
 import hashlib
+import getpass
 
 def hash_password(password, salt):
     # Concatenate password and salt, then hash
@@ -11,11 +13,10 @@ def hash_password(password, salt):
     return hashed_password
 
 def main():
-    password = input("Enter your password: ")
+    password = getpass.getpass("Enter your password: ")
     salt = "somesalt"  # You can generate a random salt if you want
     hashed_password = hash_password(password, salt)
     print("Hashed Password:", hashed_password)
 
 if __name__ == "__main__":
     main()
-
